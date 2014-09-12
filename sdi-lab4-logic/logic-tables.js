e935d9984402d590806e5133cce1ec82cdc0de5e
stavoalnso94 <galonso@fullsail.edu>
2014-09-12 15:23:43 -0400

    Lab Logic 4

---
+++ sdi-lab4-logic/logic-tables.js
@@ -0,0 +1,85 @@
+//  NAME:  Gustavo Alonso
+//  DATE:  September 11, 2014
+//  Scalable Data Infrastructures
+//  Day 4 Lab
+//  Assignment 1
+//  Using logical operators
+
+// var p;
+// var q;
+// var r;
+// var k = 0;
+
+// while(k < 8)  {
+// p = confirm("First value:  Click OK for true or Cancel for false.");
+// q = confirm("Second value:  Click OK for true or Cancel for false.");
+// r = confirm("Third value:   Click OK for true or Cancel for false.");
+// 
+// if (p || (q || r)) {
+    // console.log("With " + p + " and " + q + " and " + r + ", the outcome is TRUE.");
+// } else {
+    // console.log("With " + p + " and " + q + " and " + r + ", the outcome is FALSE.");
+// };
+
+// k++;//}
+
+
+// PROBLEM 1
+
+// var numberOfGuests = 2;
+// var childDiscount;
+// var elderDiscount;
+// var ticketPrice   = 12.00;
+// 
+// console.log("Welcome to the theatre, we have a discount for those below 10, and those over 55, otherwise you have to pay " + ticketPrice + ".");
+// 
+// childDiscount = prompt("How old are you? \nEnter your age", "10");
+// 
+// if (childDiscount <= 10) {
+	// console.log("You will recieve a discount of 7.00!");
+// } else {
+	// console.log("You have to pay the 12.00");
+// }
+// 
+// elderDiscount = prompt("How old are you? \nEnter your age", "55");
+// 
+// if (elderDiscount >= 55) {
+	// console.log("You too, qualify for our 7.00 discount!");
+// } else {
+	// console.log("You have to pay the 12.00");
+// }
+// 
+// console.log("Enjoy your movie!");
+
+
+// PROBLEM 2
+ 
+// var frontTiresPSI = 25;
+// var BackTiresPSI  = 35;
+// 
+// console.log("The two front tires have " + frontTiresPSI + " psi.");
+// console.log("The two back tires have "  + BackTiresPSI  + " psi.");
+// 
+// while (frontTiresPSI = 35) {
+// 		
+		// frontTiresPSI = confirm("You want your front tires to have the same pressure as back?");
+// 			
+// if (frontTiresPSI === true) {
+// 
+	// frontTiresPSI = frontTiresPSI + 1;
+	// console.log("Your tires are being filled.");
+	// console.log("Your tires match pressure! Have a good day.");
+	// break;
+// } else {
+	// console.log("Okay, have a good one!");
+	// break;
+// }
+// 
+// 
+// 	
+// }
+
+
+
+
+
